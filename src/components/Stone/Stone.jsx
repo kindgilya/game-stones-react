81caced41ef4b5d238e8ce9627e3c853dd623304
Elena <ed41658@gmail.com>
2024-11-07 10:41:45 +0500

    create start templates and create data

---
+++ src/components/Stone/Stone.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Stone = () => {
+  return (
+    <div>Stone</div>
+  )
+}
+
+export default Stone;
---
+++ src/data.js
@@ -0,0 +1,84 @@
+const data = [
+    {
+      id: 346,
+      color: "blue",
+      img: "t2.png",
+    },
+    {
+      id: 2345,
+      color: "red",
+      img: "t1.png",
+    },
+    {
+      id: 7,
+      color: "green",
+      img: "t3.png",
+    },
+    {
+      id: 235,
+      color: "violet",
+      img: "t4.png",
+    },
+    {
+      id: 567,
+      color: "blue",
+      img: "t2.png",
+    },
+    {
+      id: 67,
+      color: "red",
+      img: "t1.png",
+    },
+    {
+      id: 43,
+      color: "green",
+      img: "t3.png",
+    },
+    {
+      id: 87,
+      color: "violet",
+      img: "t4.png",
+    },
+    {
+      id: 95,
+      color: "blue",
+      img: "t2.png",
+    },
+    {
+      id: 352,
+      color: "red",
+      img: "t1.png",
+    },
+    {
+      id: 3456,
+      color: "green",
+      img: "t3.png",
+    },
+    {
+      id: 323446,
+      color: "violet",
+      img: "t4.png",
+    },
+    {
+      id: 6587,
+      color: "blue",
+      img: "t2.png",
+    },
+    {
+      id: 89,
+      color: "red",
+      img: "t1.png",
+    },
+    {
+      id: 6507,
+      color: "green",
+      img: "t3.png",
+    },
+    {
+      id: 2146,
+      color: "violet",
+      img: "t4.png",
+    },
+  ];
+
+  export default data;
